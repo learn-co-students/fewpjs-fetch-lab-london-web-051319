dbba4fe56c9d3f242bcc2d688d4de4f82c2ec7f8
Alex Johnston <alexjohnston@ymail.com>
2019-06-26 12:30:30 +0100

    Done.

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+//the character in the series by number
+
+function fetchCharactersByNumber(number) {
+  return fetch(`https://anapioficeandfire.com/api/characters/${number}`)
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+}
+
+//total pages
+
+function countPages(json) {
+  let pages = 0;
+  json.forEach(book => {
+    pages += book["numberOfPages"];
+    console.log(pages);
+  })
+}
+
+function totalPages() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(countPages);
+}
+
